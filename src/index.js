cee1447b73d1823482138b656f2dfec9f1b57c15
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-22 17:37:42 -0300

    Add generic content loader function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import "./style.css";
 import createHome from "./home.js";
 
 const contentLoader = (function () {
+    function loadContent(container, contentGeneratorFunction) {
+        container.innerHTML = "";
+        container.appendChild(contentGeneratorFunction());
+    };
+
     const contentContainer = document.querySelector("#content");
 
-    contentContainer.appendChild(createHome());
+    loadContent(contentContainer, createHome);
 })();
