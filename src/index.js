b5dee5afa78aed49946fc4f507bd71078f83d008
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-22 17:38:32 -0300

    Add buttons event listeners

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ const contentLoader = (function () {
     };
 
     const contentContainer = document.querySelector("#content");
+    const homeBtn = document.querySelector(".home-btn");
+    const menuBtn = document.querySelector(".menu-btn");
+    const aboutBtn = document.querySelector(".about-btn");
 
+    homeBtn.addEventListener("click", () => loadContent(contentContainer, createHome));
+    menuBtn.addEventListener("click", () => loadContent(contentContainer, createMenu));
     loadContent(contentContainer, createHome);
 })();
