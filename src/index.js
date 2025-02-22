c3eae979924af80ad4138e95c6e6a137ccedbc48
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-21 21:15:46 -0300

    Add content loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+import createHome from "./home.js";
+
+const contentLoader = (function () {
+    const contentContainer = document.querySelector("#content");
+
+    contentContainer.appendChild(createHome());
+})();
